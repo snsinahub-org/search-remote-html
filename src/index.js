1c54c6c06f1549c708ea5ef1872be727e8f22e1d
root <root@localhost.localdomain>
2022-12-21 14:25:23 -0500

    ignore-ssl

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const fs = require('fs');
 const axios = require('axios');
+const axiosRetry = require('axios-retry');
 
 async function run() {
     let found = false
     const url = core.getInput('url');
     const search = core.getInput('search');
-    console.log(`url: ${url} -- search: ${search}`)
-    const response = await axios.get(url);
+    const ignoreSSL = core.getInput('ignore-ssl');
+    console.log(`url: ${url} -- search: ${search}`);
+    const rejectUnauthorized = true
+    if(ignoreSSL == 'true') {
+        rejectUnauthorized = false
+    }
+    axiosRetry(axios, { retries: 3 });
+    const instance = axios.create({
+        httpsAgent: new https.Agent({  
+          rejectUnauthorized: rejectUnauthorized
+        })
+      });
+    const response = await instance.get(url);
     let html = response.data
     found = html.includes(search)
     console.log("HTML, ", response.data)
