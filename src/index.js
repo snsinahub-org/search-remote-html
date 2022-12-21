d169e34431492eb534e0db082b6be5f219c8c4e5
root <root@localhost.localdomain>
2022-12-21 15:21:18 -0500

    ignore-ssl

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ async function run() {
     }
     // console.log("HTML, ", response.data)
     fs.appendFileSync(process.env.GITHUB_OUTPUT, "found=" + found);
-    fs.appendFileSync(process.env.GITHUB_OUTPUT, "httpStatusCode=" + response.status);
+    // fs.appendFileSync(process.env.GITHUB_OUTPUT, "httpStatusCode=" + response.status);
 
     
 }
