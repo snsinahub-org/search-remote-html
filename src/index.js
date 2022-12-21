7af3df6aa003d76d2b816306c9208c08d185141e
root <root@localhost.localdomain>
2022-12-21 10:26:51 -0500

    add retry

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const fs = require('fs');
 const axios = require('axios');
+const axiosRetry = require('axios-retry');
 
 async function run() {
     let found = false
     const url = core.getInput('url');
     const search = core.getInput('search');
-    console.log(`url: ${url} -- search: ${search}`)
+    console.log(`url: ${url} -- search: ${search}`);
+    axiosRetry(axios, { retries: 3 });
     const response = await axios.get(url);
     let html = response.data
     found = html.includes(search)
